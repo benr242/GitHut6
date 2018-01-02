3cbe05ff02b6406b81f99a5536106b22968a7a33
Benjamin Rose <benr242@localhost.localdomain>
2018-01-01 23:35:57 -0500

    ext js asset

---
+++ web/js/onclick.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(".division li").click(function(){
+        $(this).addClass('active')
+            .siblings()
+            .removeClass('active');
+
+        //OR
+        //$('.active').removeClass('active');
+        //$(this).addClass('active');
+
+        //OR
+        $(this).addClass('active');
+        $('li').not(this).removeClass('active');
+
+        //works--
+        // alert($(this).children('a').text());
+
+        //alert($(this).children(a).text());
+
+
+
+    });
+});
