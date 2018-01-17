5557f399bdf612f5ae0cb0b6d6d8c7df2b934ce1
Benjamin Rose <benr242@localhost.localdomain>
2018-01-17 07:03:11 -0500

    starting failing assets

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    //$("#demo").html("Hello, World!");
+    $("#demo").html("Hello, World!!!!");
+
+    $("#demo").click(function () {
+        //$(this).hide();
+        $(this).addClass("blue");
+    })
+})
