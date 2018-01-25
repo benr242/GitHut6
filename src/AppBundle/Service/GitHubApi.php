7d5d401ae161c16997f8bea92a3c05e15c66fd08
Benjamin Rose <benr242@localhost.localdomain>
2018-01-25 12:06:29 -0500

    aue

--- src/AppBundle/Service/GitHubApi.php
+++ src/AppBundle/Service/GitHubApi.php
@@ -17,7 +17,7 @@ class GitHubApi
      */
     private $httpClient;
     private $myClient;
-//555555555555555555555555
+
     //public function __construct(HttpClientInterface $myClient)
     //{
         //$this->httpClient = $client;
